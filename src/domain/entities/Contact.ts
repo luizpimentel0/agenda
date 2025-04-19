918499c12caf3fb55f1c28389b8d38d44ec8429b
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-04-19 18:00:41 -0300

    feat(entities): create contact entity

---
+++ src/domain/entities/Contact.ts
@@ -0,0 +1,22 @@
+export class Contact {
+  id?: number;
+  phone: string;
+  message_phone?: string;
+  email: string;
+  address: string;
+  user_id: number;
+
+  constructor({ phone, message_phone, email, address, user_id }: {
+    phone: string,
+    message_phone?: string,
+    email: string,
+    address: string,
+    user_id: number,
+  }) {
+    this.phone = phone;
+    this.message_phone = message_phone;
+    this.email = email;
+    this.user_id = user_id;
+    this.address = address;
+  }
+}
